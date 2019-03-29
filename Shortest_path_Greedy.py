62196e58cca4483898d1bed4c8a099c1f4bc6554
arieftyo <36990643+arieftyo@users.noreply.github.com>
2019-03-30 02:31:04 +0700

    ubah_code

--- Shortest_path_Greedy.py
+++ Shortest_path_Greedy.py
@@ -7,17 +7,17 @@ grp = {}
 grp["Nagasaki"] = [["Yokohama", 80], ["Hiroshima", 97], ["Akihabara", 146]]
 grp["Kyoto"] = [["Yokohama", 99], ["Osaka", 211]]
 grp["Hiroshima"] = [["Nagasaki", 97], ["Osaka", 101], ["Akihabara", 138]]
-grp["Fuji"] = [["Iasi", 87]]
-grp["Iasi"] = [["Fuji", 87], ["Nagano", 92]]
-grp["Nagano"] = [["Iasi", 92], ["Funabashi", 142]]
+grp["Fuji"] = [["Kumamoto", 87]]
+grp["Kumamoto"] = [["Fuji", 87], ["Nagano", 92]]
+grp["Nagano"] = [["Kumamoto", 92], ["Funabashi", 142]]
 grp["Matsuyama"] = [["Osaka", 90]]
 grp["Funabashi"] = [["Osaka", 85], ["Chiba", 98], ["Nagano", 142]]
-grp["Chiba"] = [["Funabashi", 98], ["Eforie", 86]]
+grp["Chiba"] = [["Funabashi", 98], ["Nara", 86]]
 grp["Osaka"] = [["Chiba", 86]]
 grp["Tokyo"] = [["Yokohama" , 140], ["Saitama", 118], ["Sakai", 75]]
-grp["Yokohama"] = [["Oradea" , 151], ["Tokyo" , 140], ["Kyoto" , 99], ["Nagasaki" , 80]]
-grp["Sakai"] = [["Tokyo" , 75], ["Oradea", 71]]
-grp["Oradea"] = [["Sakai" , 71], ["Yokohama", 151]]
+grp["Yokohama"] = [["Atsugi" , 151], ["Tokyo" , 140], ["Kyoto" , 99], ["Nagasaki" , 80]]
+grp["Sakai"] = [["Tokyo" , 75], ["Atsugi", 71]]
+grp["Atsugi"] = [["Sakai" , 71], ["Yokohama", 151]]
 grp["Saitama"] = [["Tokyo", 118], ["Sendai", 118]]
 grp["Sendai"] = [["Saitama", 111], ["Fukuyama", 70]]
 grp["Fukuyama"] = [["Sendai", 70], ["Kashiwa", 75]]
@@ -30,15 +30,15 @@ heuristics = {
     "Osaka"          : 0,
     "Akihabara"      : 160,
     "Kashiwa"        : 242,
-    "Eforie"         : 161,
+    "Nara"           : 161,
     "Kyoto"          : 178,
     "Matsuyama"      : 77,
     "Chiba"          : 151,
-    "Iasi"           : 226,
+    "Kumamoto"       : 226,
     "Sendai"         : 244,
     "Fukuyama"       : 241,
     "Fuji"           : 234,
-    "Oradea"         : 380,
+    "Atsugi"         : 380,
     "Hiroshima"      : 98,
     "Nagasaki"       : 193,
     "Yokohama"       : 253,
